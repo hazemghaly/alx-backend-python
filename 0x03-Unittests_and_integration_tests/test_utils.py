daedcfd86d9e11f2d278583b11431afa8f4f9d1a
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-04-06 03:15:32 +0200

    Add files via upload

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Generic utilities for github org client.
+"""
+import unittest
+import utils
+from parameterized import parameterized
+
+
+def access_nested_map(nested_map, path):
+    """function"""
+    try:
+        for key in path:
+            nested_map = nested_map[key]
+        return nested_map
+    except KeyError:
+        return None
+
+
+class TestAccessNestedMap (unittest.TestCase):
+    """ class testing
+    """
+    @parameterized.expand([
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2)
+        ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        """test function"""
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected_result)
