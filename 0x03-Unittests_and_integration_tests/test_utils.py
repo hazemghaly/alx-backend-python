4ae3b7c32b2028c44bd2e6d8ada51a8ee4dafa68
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-04-11 19:30:25 +0200

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 """Generic utilities for github org client.
 """
-import unittest
+from utils import access_nested_map
 from parameterized import parameterized
+import unittest
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """ class testing
     """
+
     @parameterized.expand([
-            ({"a": 1}, ("a",), 1),
-            ({"a": {"b": 2}}, ("a",), {"b": 2}),
-            ({"a": {"b": 2}}, ("a", "b"), 2),
-            ({}, ("a",), "Key not found: 'a'"),
-            ({"a": 1}, ("a", "b"), "Key not found: 'b'")
-        ])
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({}, ("a",), "Key not found: 'a'"),
+        ({"a": 1}, ("a", "b"), "Key not found: 'b'")
+    ])
     def test_access_nested_map(self, nested_map, path, expected_result):
         """test function"""
         result = access_nested_map(nested_map, path)
