1d5a18b912562499a8f3913a4016063f2556af1b
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-04-11 19:50:11 +0200

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,19 +9,27 @@ import unittest
 class TestAccessNestedMap(unittest.TestCase):
     """ class testing
     """
-
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
-        ({}, ("a",), "Key not found: 'a'"),
-        ({"a": 1}, ("a", "b"), "Key not found: 'b'")
     ])
+
+
+    
     def test_access_nested_map(self, nested_map, path, expected_result):
         """test function"""
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected_result)
 
-    def test_access_nested_map_exception(self):
-        """test function"""
-        self.assertRaises(KeyError)
+    # # def test_access_nested_map_exception(self):
+    # #     """test function"""
+    # #     self.assertRaises(ValueError)
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test function for exception"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
